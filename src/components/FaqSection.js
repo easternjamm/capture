afdda038b5b55dbfaf34dfb769646d9b24c41734
EasternJamm <apetremarianioan@gmail.com>
2022-10-24 14:46:23 +0300

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,51 @@
+const FaqSection = () => {
+  return (
+    <div className='faq'>
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className='question'>
+        <h4>How Do I Start?</h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque,
+            blanditiis!
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>Daily schedule</h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque,
+            blanditiis!
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>Different Payment Methods</h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque,
+            blanditiis!
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>What products do you offer?</h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque,
+            blanditiis!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
