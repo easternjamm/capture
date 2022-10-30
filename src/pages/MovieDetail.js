5e64bf66196654aebe21ef23f5e878248ee04472
EasternJamm <apetremarianioan@gmail.com>
2022-10-30 18:23:21 +0200

    16. Final

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -89,6 +89,10 @@ const Awards = styled.div`
   margin: 5rem 10rem;
   align-items: center;
   justify-content: space-around;
+  @media (max-width: 1500px) {
+    display: block;
+    margin: 2rem 2rem;
+  }
 `;
 const ImageDisplay = styled.div`
   min-height: 50vh;
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import athlete from "../img/athlete-small.png";
 import theracer from "../img/theracer-small.png";
 import goodtimes from "../img/goodtimes-small.png";
+import ScrollTop from "../components/ScrollTop";
 
 import { motion } from "framer-motion";
 import {
@@ -14,8 +15,11 @@ import {
   pageAnimation,
   lineAnim,
 } from "../animation";
+import { useScroll } from "../components/useScroll";
 
 const OurWork = () => {
+  const [element, controls] = useScroll();
+  const [element2, controls2] = useScroll();
   return (
     <Work
       variants={pageAnimation}
@@ -39,20 +43,26 @@ const OurWork = () => {
           </Hide>
         </Link>
       </Movie>
-      <Movie>
+      <Movie ref={element} variants={fade} animate={controls} initial='hidden'>
         <h2>The Racer</h2>
-        <div className='line'></div>
+        <motion.div variants={lineAnim} className='line'></motion.div>
         <Link to='/work/the-racer'>
           <img src={theracer} alt='theracer' />
         </Link>
       </Movie>
-      <Movie>
+      <Movie
+        ref={element2}
+        variants={fade}
+        animate={controls2}
+        initial='hidden'
+      >
         <h2>Good times</h2>
-        <div className='line'></div>
+        <motion.div variants={lineAnim} className='line'></motion.div>
         <Link to='/work/good-times'>
           <img src={goodtimes} alt='goodtimes' />
         </Link>
       </Movie>
+      <ScrollTop />
     </Work>
   );
 };
@@ -64,8 +74,11 @@ const Work = styled(motion.div)`
   h2 {
     padding: 1rem 0rem;
   }
+  @media (max-width: 1300px) {
+    padding: 2rem 2rem;
+  }
 `;
-const Movie = styled.div`
+const Movie = styled(motion.div)`
   padding-bottom: 10rem;
   .line {
     height: 0.5rem;
