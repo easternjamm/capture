91ad4276517c563fd4724c64b539bcf8ad27ef7b
EasternJamm <apetremarianioan@gmail.com>
2023-08-23 17:39:40 +0300

    4. Nav bar and routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+
+import { Routes, Route } from "react-router-dom";
 
 import GlobalStyle from "./components/GlobalStyle";
 
@@ -6,7 +11,12 @@ function App() {
   return (
     <div className='App'>
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Routes>
+        <Route path='/' element={<AboutUs />}></Route>
+        <Route path='/work' element={<OurWork />}></Route>
+        <Route path='/contact' element={<ContactUs />}></Route>
+      </Routes>
     </div>
   );
 }
